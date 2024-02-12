a4887e86cdb092eed86aff9cb10c3c7f85d0a86b
Adriano Weverton <adriwco@gmail.com>
2024-02-12 01:23:10 -0300

    function delete note

--- src/App.tsx
+++ src/App.tsx
@@ -29,13 +29,21 @@ export default function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string){
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>){
     const query = event.target.value
     setSearch(query)
   }
   
   const filteredNotes = search != ''
-    ? notes.filter(note => note.content.includes(search))
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
     : notes
 
   return (
@@ -55,7 +63,7 @@ export default function App() {
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {
           filteredNotes.map((note)=>{
-            return <NoteCard key={note.id} note={note}/>
+            return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>
           })
         }
 
